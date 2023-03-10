f331302ed18e07c79f27f4c657a1f78daf0e3206
Willyjvp <willy.vas.p@gmail.com>
2023-03-10 09:36:08 -0600

    initial blind75 exercises

---
+++ blind75/containsDuplicate.js
@@ -0,0 +1,37 @@
+/*
+217. Contains Duplicate
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+=========================================
+              SOLUTION
+=========================================
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  const result = [];
+  for (const value of nums) {
+    if (result[value] !== undefined) return true;
+    result[value] = 1;
+  }
+  return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
---
+++ blind75/twoSum.js
@@ -0,0 +1,48 @@
+/*
+1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+ 
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+//=========================================
+              SOLUTION
+//=========================================
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let result = [];
+  for (const [key, value] of nums.entries()) {
+    //second number that we are looking
+    if (result[value] !== undefined) return [result[value], key];
+    //saving the key to return when find the other number
+    //where the position is the number that we are looking
+    result[target - value] = key;
+  }
+  return [0];
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
